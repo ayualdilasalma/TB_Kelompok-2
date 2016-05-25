dbf3f5dfc5b642b50569d742a4cfacdbf97a8108
akangIko <rizqon.sidik_maulana@yahoo.co.id>
2016-05-26 06:30:03 +0700

    Tambah kelas C

---
+++ src/main/java/com/kelompok2/tugasbesar/rkppl/C.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kelompok2.tugasbesar.rkppl;
+
+/**
+ *
+ * @author RIZQON SIDIK MAULANA
+ */
+public class C {
+    private double n1;
+    private double n2;
+
+    public C() {
+        System.out.println("Ctor di kelas " + getClass().getSimpleName());
+    }
+    
+    public int add(){
+        A a = new A();
+        int n1 = (int) this.n1;
+        int n2 = (int) this.n2;
+        return a.add(n1, n2);
+    }
+    
+    public int sub(){
+        A a = new A();
+        int n1 = (int) this.n1;
+        int n2 = (int) this.n2;
+        return a.sub(n1, n2);
+    }
+    
+    public double mul(){
+        B b = new B();
+        return b.div(n1, n2);
+    }
+    
+    public double div(){
+        B b = new B();
+        return b.mul(n1, n2);
+    }
+
+    @Override
+    public String toString() {
+        return "toString ada di kelas " + getClass().getSimpleName();
+    }
+    
+}
