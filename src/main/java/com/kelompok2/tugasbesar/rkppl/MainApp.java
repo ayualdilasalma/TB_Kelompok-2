ee82512a1802ee92c48b6452e2e6b2e4e15b696b
Muhammad <Muhammad@QOSMIO>
2016-05-26 11:43:07 +0700

    Pemanbahan komentar pada kelas MainApp

--- src/main/java/com/kelompok2/tugasbesar/rkppl/MainApp.java
+++ src/main/java/com/kelompok2/tugasbesar/rkppl/MainApp.java
@@ -12,16 +12,24 @@ package com.kelompok2.tugasbesar.rkppl;
 public class MainApp {
 
     public static void main(String[] args) {
+        //Membuat variable n,n1,n2 dan memberikan nilai pada masing2 variable
         int n = 7;
         int n1 = 8;
         int n2 = 9;
 
+        //Instansiasi Kelas D
         D d = new D();
+        //Pemanggilan method pow pada Kelas D
         System.out.println(d.pow(n1, n2));
+        //Pemanggilan method mul pada Kelas D
         System.out.println(d.mul(n1, n2));
+        //Pemanggilan method div pada Kelas D
         System.out.println(d.div(n1, n2));
+        //Pemanggilan method toString pada Kelas D
         System.out.println(d.toString());
-        
+        System.out.println("");
+
+        //Instansiasi Kelas C
         C c = new C();
     }
 }
