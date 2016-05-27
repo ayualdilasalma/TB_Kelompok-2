f78668569b8a49868be8c775146ad3a82c42f02e
Muhammad <Muhammad@QOSMIO>
2016-05-27 14:31:44 +0700

    Menambah komentar pada Kelas E

--- src/main/java/com/kelompok2/tugasbesar/rkppl/E.java
+++ src/main/java/com/kelompok2/tugasbesar/rkppl/E.java
@@ -12,22 +12,45 @@ import java.util.List;
  * @author Ginan
  */
 public class E {
+
     private List dataList;
 
+    /**
+     * Konstruktor yang menampilkan output Ctor di kelas D
+     */
     public E() {
         System.out.println("Ctor di kelas " + getClass().getSimpleName());
     }
 
+    /**
+     * Mengecek apakah dari data yang ada mengandung kata sesuai parameter
+     */
+    public boolean contains(String n) {
+        return n.contains(n);
+    }
+
+    /**
+     * Method get berfungsi sebagai pemberi nilai dari attribut Kelas ini dari
+     * kelas lain yang meminta
+     */
     public List getDataList() {
         return dataList;
     }
 
+    /**
+     * Method set berfungsi sebagai method yang menerima nilai dari kelas lain
+     * yang nantinya akan dimasukkan kedalam attribut yang ada di Kelas ini
+     */
     public void setDataList(List dataList) {
         this.dataList = dataList;
     }
-    
+
+    /**
+     * Method toString adalah method yang akan menampilkan output toString di
+     * kelas E
+     */
     @Override
     public String toString() {
-        return "toString di kelas " +getClass().getSimpleName();
+        return "toString di kelas " + getClass().getSimpleName();
     }
 }
