79e878ce46afe524da6f8c88df4a1c32d4ba099f
Restu <Restu@Restu-PC>
2016-05-27 15:00:27 +0700

    penambahan komentar keseluruhan

--- src/main/java/com/kelompok2/tugasbesar/rkppl/AddAndSubInterface.java
+++ src/main/java/com/kelompok2/tugasbesar/rkppl/AddAndSubInterface.java
@@ -10,11 +10,30 @@ package com.kelompok2.tugasbesar.rkppl;
  * @author Restu
  *
  */
-
 //interface ini merupakan pembuatan interface AddAndSubInterface
 public interface AddAndSubInterface {
 
-    public int add(int n1, int n2); // pembuatan method add yang berguna untuk menambahkan nilai n1 dan n2
-    public int sub(int n1, int n2);
+    public int add(int n1, int n2);
+    /* Ini merupakan method sub yang untuk kelas A
+     * 
+     * method ini berfungsi untuk menjumlahkan dua bilangin
+     * @param n1 merupakan param pertama method sub sebagai
+     *           bilangan pengurang
+     * @param n2 merupakan param kedua method sub sebagai
+     *           bilangan yang dikurangkan dengan n1
+    
+    */
+    public int sub(int n1, int n2); 
+  
+    /* Ini merupakan method implementasi dari method sub untuk kelas A
+    /* 
+     * method ini berfungsi untuk mengurangi dua bilangin
+     * @param n1 meruapakn param pertama method sub sebagai
+     *           bilangan pengurang
+     * @param n2 merupakan param kedua method sub sebagai
+     *           bilangan yang dikurangkan dengan n1
+     * 
+    
+     */
 
 }
