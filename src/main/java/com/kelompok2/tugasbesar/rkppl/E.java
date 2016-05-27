95bf87cbc5ccb0aa36fb72283ffebb2686c9d797
Muhammad <Muhammad@QOSMIO>
2016-05-27 14:14:10 +0700

    Membuat Kelas E

---
+++ src/main/java/com/kelompok2/tugasbesar/rkppl/E.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kelompok2.tugasbesar.rkppl;
+
+import java.util.List;
+
+/**
+ *
+ * @author Ginan
+ */
+public class E {
+    
+}
