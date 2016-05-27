2032390de4383e6f86a86769692679e19c73b637
akangIko <rizqon.sidik_maulana@yahoo.co.id>
2016-05-27 18:27:25 +0700

    Penambahan komentar pada setiap method di kelas B dan C

--- src/main/java/com/kelompok2/tugasbesar/rkppl/B.java
+++ src/main/java/com/kelompok2/tugasbesar/rkppl/B.java
@@ -10,21 +10,34 @@ package com.kelompok2.tugasbesar.rkppl;
  * @author RIZQON SIDIK MAULANA
  */
 public class B implements MulAndDivInterface {
-
+    /**
+     * Konstruktor untuk menampilkan Ctor di kelas B
+     */
     public B() {
         System.out.println("Ctor di kelas " + getClass().getSimpleName());
     }
-
+    
+    /**
+     * Method ini merupakan method implementasi dari kelas MulAndDivInterface
+     * method ini akan melakukan proses perkalian antara n1 dan n2
+     */
     @Override
     public double mul(double n1, double n2) {
         return n1 * n2;
     }
-
+    
+    /**
+     * Method ini merupakan method implementasi dari kelas MulAndDivInterface
+     * method ini akan melakukan proses pembagian antara n1 dan n2
+     */
     @Override
     public double div(double n1, double n2) {
         return n1 / n2;
     }
-
+    
+    /**
+     * Method ini akan menampilkan output toString di kelas B
+     */
     @Override
     public String toString() {
         return "toString di kelas " + getClass().getSimpleName();
--- src/main/java/com/kelompok2/tugasbesar/rkppl/C.java
+++ src/main/java/com/kelompok2/tugasbesar/rkppl/C.java
@@ -13,53 +13,87 @@ public class C extends Math {
     private double n1;
     private double n2;
 
+/**  Konstruktor yang menampilkan Ctor di kelas C*/
     public C() {
         System.out.println("Ctor di kelas " + getClass().getSimpleName());
     }
-    
-    public int add(){
+
+    /**
+     * Method untuk menambah variable n1 dan n2 dimana method penjumlahannya di
+     * panggil di kelas A dan variable n1 dan n2 dikonversi kan terlebih dahulu
+     * ke integer sesuai dengan method penjumlahan yang ada di kelas A
+     */
+    public int add() {
         A a = new A();
         int n1 = (int) this.n1;
         int n2 = (int) this.n2;
         return a.add(n1, n2);
     }
-    
-    public int sub(){
+
+    /**
+     * Method untuk pengurangan variable n1 dan n2 dimana method pengurangan
+     * dipanggil di kelas A dan variable n1 dan n2 dikonversi kan terlebih
+     * dahulu ke integer sesuai dengan method pengurangan yang ada di kelas A
+     */
+    public int sub() {
         A a = new A();
         int n1 = (int) this.n1;
         int n2 = (int) this.n2;
         return a.sub(n1, n2);
     }
-    
-    public double mul(){
+
+    /**
+     * Method perkalian variable n1 dan n2 dimana method perkalian dipanggil
+     * dari kelas B
+     *
+     */
+
+    public double mul() {
         B b = new B();
         return b.div(n1, n2);
     }
-    
-    public double div(){
+
+    /**
+     * Method pembagian variable n1 dan n2 dimana method pembagian dipanggil
+     * dari kelas B
+     */
+    public double div() {
         B b = new B();
         return b.mul(n1, n2);
     }
 
+    /**
+     * Method untuk mengambil nilai dari variable N1
+     */
     public double getN1() {
         return n1;
     }
 
+    /**
+     * Method untuk mengisi nilai di variable N1
+     */
     public void setN1(double n1) {
         this.n1 = n1;
     }
 
+    /**
+     * Method untuk mengambil nilai dari variable N2
+     */
     public double getN2() {
         return n2;
     }
-
+    /**
+     * Method untuk mengisi nilai di variable N2
+     */
     public void setN2(double n2) {
         this.n2 = n2;
     }
-    
+    /**
+     * Method untuk menampilkan output toString ada di kelas C
+     */
     @Override
     public String toString() {
         return "toString ada di kelas " + getClass().getSimpleName();
     }
-    
+
 }
