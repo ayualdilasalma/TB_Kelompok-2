d0552b75ce4a0c844c622523e5c6ff1f43be2721
Badrus Salam <Badrus Salam@Assalam>
2016-05-28 15:06:29 +0700

    Menambahkan Komentar & Penjelasan Pada Kelas TestingB

--- src/test/java/testing/TestingB.java
+++ src/test/java/testing/TestingB.java
@@ -17,23 +17,39 @@ import org.junit.Test;
  * @author Badrus Salam
  */
 public class TestingB {
+    // instansiasi variabel b dengan tipe data B
     private B b;
+    /*
+     * method startTest dieksekusi sebelum test dimulai
+     * dalam method ini dilakukan instansiasi objek yang akan dites
+    */
     @Before
      public void startTest() {
         // inisialisasi objek b dengan tipe objek B
         b = new B();
         System.out.println("Mengawali Testing Untuk Kelas B");
     }
+      /*
+    * method testCase1 berisi untuk menguji kebenaran nilai
+    * yang dihasilkan dari method add dalam kelas B
+    */
       @Test
     public void testCase1() {
         double jum = b.mul(4,2);
         assertTrue("Seharusnya benar", jum == 8 );
     }    
+     /*
+    * method testCase2 berisi untuk menguji kebenaran nilai
+    * yang dihasilkan dari method div dalam kelas B
+    */
       @Test
     public void testCase2() {
         double jum = b.div(10, 2);
         assertTrue("Seharusnya benar", jum == 5);
-    }  
+    } 
+     /* method endTest akan dieksekusi setelah pengujian selesai dilakukan
+     * sebagai penanda bahwa testing telah selesai dilakukan
+     */
    @After
     public void endTest() {
         System.out.println("Mengakhiri Testing Untuk Kelas B ");
